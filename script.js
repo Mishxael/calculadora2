553414b5a857e7fbd54f2a8383d54c3496e9720c
Gary Misael <masterod090@gmail.com>
2025-05-13 10:54:42 -0500

    sd

---
+++ script.js
@@ -0,0 +1,30 @@
+
+function suma(){
+    var numero1= document.getElementById("numero1").value;
+var numero2= document.getElementById("numero2").value;
+debugger
+var resultado = parseInt(numero1) + parseInt(numero2);
+document.getElementById("resultado").innerHTML="el resultado de la suma es: "+resultado;
+}
+function resta(){
+    var numero1= document.getElementById("numero1").value;
+var numero2= document.getElementById("numero2").value;
+debugger
+var resultado = parseInt(numero1) - parseInt(numero2);
+document.getElementById("resultado").innerHTML="el resultado de la suma es: "+resultado;
+}
+function multiplicacion(){
+    var numero1= document.getElementById("numero1").value;
+var numero2= document.getElementById("numero2").value;
+debugger
+var resultado = parseInt(numero1) * parseInt(numero2);
+document.getElementById("resultado").innerHTML="el resultado de la suma es: "+resultado;
+}
+function division(){
+    var numero1= document.getElementById("numero1").value;
+var numero2= document.getElementById("numero2").value;
+debugger
+var resultado = parseInt(numero1) / parseInt(numero2);
+document.getElementById("resultado").innerHTML="el resultado de la suma es: "+resultado;
+}
+
